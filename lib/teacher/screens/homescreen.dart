72aa719f39198c68e1f31dbc10a97bfc6686d730
Aswin964567 <aswinvtk2001@gmail.com>
2024-09-16 21:51:46 +0530

    responsive file added

--- lib/teacher/screens/homescreen.dart
+++ lib/teacher/screens/homescreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:school_app/admin/utils/responsive.dart';
 
 class TeacherScreen extends StatelessWidget {
   const TeacherScreen({super.key});
@@ -8,86 +8,91 @@ class TeacherScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Hi ,',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineLarge!
-                          .copyWith(fontSize: 48),
-                    ),
-                    Text(
-                      'Vincent',
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineLarge!
-                          .copyWith(color: Color(0xff555555)),
-                    ),
-                  ],
-                ),
-                Spacer(),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 30),
-                  child: Image.asset('assets/admin.png'),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 180,
-            ),
-            _customContainer(color: Colors.green, text: 'Homework'),
-            SizedBox(
-              height: 10,
-            ),
-            _customContainer(color: Colors.red, text: 'Leave Request'),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                OutlinedButton(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 25, horizontal: 55)),
-                  child: Text(
-                    'Student',
-                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                          color: Color(0xff555555),
-                          fontWeight: FontWeight.w700,
-                        ),
+        padding: EdgeInsets.symmetric(horizontal: Responsive.width * 7),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: Responsive.height * 7,
+              ),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hi ,',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge!
+                            .copyWith(fontSize: 48),
+                      ),
+                      Text(
+                        'Vincent',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge!
+                            .copyWith(color: Color(0xff555555)),
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                OutlinedButton(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 25, horizontal: 55)),
-                  child: Text(
-                    'Teacher',
-                    style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                          color: Color(0xff555555),
-                          fontWeight: FontWeight.w700,
-                        ),
+                  Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: Image.asset('assets/admin.png'),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+              SizedBox(
+                height: Responsive.height * 20,
+              ),
+              _customContainer(color: Colors.green, text: 'Homework'),
+              SizedBox(
+                height: 10,
+              ),
+              _customContainer(color: Colors.red, text: 'Leave Request'),
+              SizedBox(
+                height: Responsive.height * 2,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                            vertical: Responsive.height * 3,
+                            horizontal: Responsive.width * 12)),
+                    child: Text(
+                      'Student',
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            color: Color(0xff555555),
+                            fontWeight: FontWeight.w700,
+                          ),
+                    ),
+                  ),
+                  // SizedBox(
+                  //   width: 5,
+                  // ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                          vertical: Responsive.height * 3,
+                          horizontal: Responsive.width * 12),
+                    ),
+                    child: Text(
+                      'Teacher',
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            color: Color(0xff555555),
+                            fontWeight: FontWeight.w700,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -98,22 +103,19 @@ class TeacherScreen extends StatelessWidget {
       required String text,
       IconData icon = Icons.dashboard_customize_outlined}) {
     return Container(
-      padding: EdgeInsets.all(25),
+      padding: EdgeInsets.all(Responsive.height * 3),
       decoration: BoxDecoration(
         color: color,
         borderRadius: BorderRadius.circular(8),
       ),
       child: Row(
         children: [
-          SizedBox(
-            width: 10,
-          ),
-          Icon(
-            icon,
-            color: Colors.white,
-          ),
-          SizedBox(
-            width: 20,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: Responsive.width * 3),
+            child: Icon(
+              icon,
+              color: Colors.white,
+            ),
           ),
           Text(
             text,
