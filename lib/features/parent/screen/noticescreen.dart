d2dd88b65e22d2d2d228c3eb5a011caac29f4f12
Manuprasad01 <manupkm24@gmail.com>
2024-10-26 12:14:27 +0530

    parent api checking

--- lib/features/parent/screen/noticescreen.dart
+++ lib/features/parent/screen/noticescreen.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:school_app/base/routes/app_route_const.dart';
+import 'package:school_app/features/admin/notices/controller/notice_controller.dart';
 
 import 'package:school_app/features/parent/widgets/noticecard.dart';
 
-class NoticePage extends StatelessWidget {
+class NoticePage extends StatefulWidget {
   const NoticePage({super.key});
+
+  @override
+  State<NoticePage> createState() => _NoticePageState();
+}
+
+class _NoticePageState extends State<NoticePage> {
+  @override
+  void initState() {
+    context.read<NoticeController>().getNotices();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +43,7 @@ class NoticePage extends StatelessWidget {
         elevation: 0,
         iconTheme: const IconThemeData(color: Colors.black),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
@@ -60,26 +73,39 @@ class NoticePage extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 10),
-              NoticeCard(
-                noticeTitle: "PTA meeting class 09",
-                date: "15 - 06 - 24",
-                time: "09:00 am",
-              ),
-              NoticeCard(
-                noticeTitle: "PTA meeting class 02",
-                date: "15 - 06 - 24",
-                time: "09:00 am",
-              ),
-              NoticeCard(
-                noticeTitle: "PTA meeting class VII",
-                date: "15 - 06 - 24",
-                time: "09:00 am",
-              ),
-              NoticeCard(
-                noticeTitle: "PTA meeting class X",
-                date: "15 - 06 - 24",
-                time: "09:00 am",
-              ),
+
+              Consumer<NoticeController>(builder: (context, value, child) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: value.notices.length,
+                  itemBuilder: (context, index) {
+                    return NoticeCard(
+                        noticeTitle: value.notices[index].title ?? "",
+                        date: value.notices[index].date.toString(),
+                        time: value.notices[index].createdAt.toString());
+                  },
+                );
+              })
+              // NoticeCard(
+              //   noticeTitle: "PTA meeting class 09",
+              //   date: "15 - 06 - 24",
+              //   time: "09:00 am",
+              // ),
+              // NoticeCard(
+              //   noticeTitle: "PTA meeting class 02",
+              //   date: "15 - 06 - 24",
+              //   time: "09:00 am",
+              // ),
+              // NoticeCard(
+              //   noticeTitle: "PTA meeting class VII",
+              //   date: "15 - 06 - 24",
+              //   time: "09:00 am",
+              // ),
+              // NoticeCard(
+              //   noticeTitle: "PTA meeting class X",
+              //   date: "15 - 06 - 24",
+              //   time: "09:00 am",
+              // ),
             ],
           ),
         ),
