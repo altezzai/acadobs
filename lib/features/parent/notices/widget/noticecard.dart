76dcf32d8d195c3ab967c8617c38ae33ec326f2f
Manuprasad01 <manupkm24@gmail.com>
2024-11-19 23:46:27 +0530

    student leave request page some modification

--- lib/features/parent/notices/widget/noticecard.dart
+++ lib/features/parent/notices/widget/noticecard.dart
@@ -72,7 +72,13 @@ class NoticeCard extends StatelessWidget {
                     color: Colors.blue.withOpacity(0.1)),
                 child: Text(date, style: const TextStyle(color: Colors.blue))),
           ),
-          trailing: Text(time),
+          trailing: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.blue.withOpacity(0.1)),
+              child: Text(time, style: const TextStyle(color: Colors.blue))),
+          // trailing: Text(time),
         ),
       ),
     );
