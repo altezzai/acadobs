954eeeb0d0ef8c1f5661e5325e8b8f7b89858132
Soorya2002 <soorya1923@gmail.com>
2024-11-21 21:50:20 +0530

    donarion detailed page completed

--- lib/features/admin/payments/screens/donation_view.dart
+++ lib/features/admin/payments/screens/donation_view.dart
@@ -7,56 +7,26 @@ import 'package:school_app/base/utils/date_formatter.dart';
 import 'package:school_app/base/utils/responsive.dart';
 import 'package:school_app/core/navbar/screen/bottom_nav.dart';
 import 'package:school_app/core/shared_widgets/custom_appbar.dart';
-import 'package:school_app/features/admin/payments/model/payment_model.dart';
+import 'package:school_app/features/admin/payments/model/donation_model.dart';
 
-class PaymentView extends StatefulWidget {
-  final Payment payment;
-
-  PaymentView({required this.payment});
+class DonationView extends StatefulWidget {
+  final Donation donation;
+  DonationView({required this.donation});
 
   @override
-  State<PaymentView> createState() => _PaymentViewState();
+  State<DonationView> createState() => _DonationViewState();
 }
 
-class _PaymentViewState extends State<PaymentView> {
-  // Helper method to get color based on status
-  Color _getStatusColor(String? status) {
-    switch (status) {
-      case 'Completed':
-        return Colors.green;
-      case 'Pending':
-        return Colors.orange;
-      case 'Failed':
-        return Colors.red;
-      default:
-        return Colors.grey; // Default for unknown status
-    }
-  }
-
-  // Helper method to get icon based on status
-  IconData _getStatusIcon(String? status) {
-    switch (status) {
-      case 'Completed':
-        return Icons.check_circle;
-      case 'Pending':
-        return Icons.hourglass_empty;
-      case 'Failed':
-        return Icons.cancel;
-      default:
-        return Icons.help_outline; // Default for unknown status
-    }
-  }
-
+class _DonationViewState extends State<DonationView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             CustomAppbar(
-              title: "Payments",
+              title: 'Donations',
               isProfileIcon: false,
               onTap: () {
                 context.goNamed(
@@ -67,30 +37,29 @@ class _PaymentViewState extends State<PaymentView> {
             ),
             SizedBox(height: Responsive.height * 2),
             CircleAvatar(
-              backgroundImage: widget.payment.studentPhoto != null
-                  ? NetworkImage(widget.payment.studentPhoto)
+              backgroundImage: widget.donation.studentPhoto != null
+                  ? NetworkImage(widget.donation.studentPhoto)
                   : AssetImage('assets/child1.png') as ImageProvider,
               radius: 25,
             ),
             SizedBox(height: Responsive.height * 2),
             Text(
-              'From ${capitalizeFirstLetter(widget.payment.fullName ?? "")}',
+              'From ${capitalizeFirstLetter(widget.donation.fullName ?? "")}',
               style: textThemeData.bodyMedium,
             ),
-            Text(widget.payment.transactionId ?? ""),
+            Text(widget.donation.transactionId ?? ""),
             SizedBox(height: Responsive.height * 2),
             Container(
               padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
-                color: _getStatusColor(widget.payment.paymentStatus)
-                    .withOpacity(0.2),
+                color: Colors.green.shade100.withOpacity(0.2),
               ),
               child: Text(
-                '₹${widget.payment.amountPaid}',
+                '₹${widget.donation.amountDonated}',
                 style: TextStyle(
                   fontSize: 55,
-                  color: _getStatusColor(widget.payment.paymentStatus),
+                  color: Colors.green,
                 ),
               ),
             ),
@@ -99,19 +68,12 @@ class _PaymentViewState extends State<PaymentView> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Icon(
-                  _getStatusIcon(widget.payment.paymentStatus),
-                  color: _getStatusColor(widget.payment.paymentStatus),
+                  Icons.check_circle,
+                  color: Colors.green,
                   size: 30,
                 ),
                 SizedBox(width: Responsive.width * 2),
-                Text(
-                  widget.payment.paymentStatus ?? "Unknown Status",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: _getStatusColor(widget.payment.paymentStatus),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+                Text('Completed', style: textThemeData.bodyMedium),
               ],
             ),
             SizedBox(height: Responsive.height * 1),
@@ -122,7 +84,9 @@ class _PaymentViewState extends State<PaymentView> {
             ),
             SizedBox(height: Responsive.height * 2),
             Text(
-              '${DateFormatter.formatDateString(widget.payment.paymentDate.toString())}, ${TimeFormatter.formatTimeFromString(widget.payment.createdAt.toString())}',
+              '${DateFormatter.formatDateString(widget.donation.donationDate.toString())}  ${TimeFormatter.formatTimeFromString(
+                widget.donation.createdAt.toString(),
+              )}',
             ),
           ],
         ),
