a4408aacbe5ec74627a7654fe289e5da69afce65
Aiswarya151 <aiswaryashaji55@gmail.com>
2024-12-12 14:02:02 +0530

    student and teacher reports get implemented

--- lib/features/admin/reports/services/teacher_report_services.dart
+++ lib/features/admin/reports/services/teacher_report_services.dart
@@ -3,11 +3,15 @@ import 'package:school_app/base/services/api_services.dart';
 
 class TeacherReportServices {
 
-  Future<Response> getTeacherReports() async{
+  Future<Response> getTeacherReport() async{
     final Response response= await ApiServices.get("/teacherReport");
     return response;
   }
 
- 
+ Future<Response> getTeacherReportByNameAndDate({required String teacherName, required String startDate,required String endDate}) async {
+    final Response response = await ApiServices.get("/teacherReport/?teacher_name=$teacherName&start_date=$startDate&end_date=$endDate");
+    return response;
+  }
+
   
 }
