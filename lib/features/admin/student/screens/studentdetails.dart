2745e2bf7945e9dcf7a5f21f12c40ef9292be470
Aswin964567 <aswinvtk2001@gmail.com>
2024-12-14 23:15:06 +0530

    fixes

--- lib/features/admin/student/screens/studentdetails.dart
+++ lib/features/admin/student/screens/studentdetails.dart
@@ -140,7 +140,7 @@ class _StudentDetailPageState extends State<StudentDetailPage> {
                         ],
                       ),
                     ),
-                  ),
+                  )
                 ),
               ];
             },
