eab81f3af311eac7b10e57d4f260fa5f9beb19a8
Aiswarya151 <aiswaryashaji55@gmail.com>
2025-02-08 13:13:34 +0530

    changes in admin section teachers page and subjects page

--- lib/features/admin/subjects/widgets/subject_tile.dart
+++ lib/features/admin/subjects/widgets/subject_tile.dart
@@ -7,8 +7,8 @@ class SubjectTile extends StatelessWidget {
   final String iconPath;
   final Color iconColor;
   final VoidCallback onEdit;
-  final double? topRadius;
-  final double? bottomRadius;
+  final double topRadius;
+  final double bottomRadius;
 
   const SubjectTile({
     Key? key,
@@ -17,50 +17,125 @@ class SubjectTile extends StatelessWidget {
     required this.iconPath,
     required this.iconColor,
     required this.onEdit,
-    this.topRadius,
-    this.bottomRadius,
+    this.topRadius=10,
+    this.bottomRadius=10,
   }) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 2, horizontal: 2),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(topRadius ?? 8),
-          topRight: Radius.circular(topRadius ?? 8),
-          bottomLeft: Radius.circular(bottomRadius ?? 8),
-          bottomRight: Radius.circular(bottomRadius ?? 8),
+    return 
+      
+      Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(bottomRadius),
+              bottomRight: Radius.circular(bottomRadius),
+              topLeft: Radius.circular(topRadius),
+              topRight: Radius.circular(topRadius)),
         ),
-        side: BorderSide(
-          color: Colors.grey.shade300,
-          width: 1.0,
-        ),
-      ),
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundColor: iconColor.withOpacity(0.2),
-          child: Image.asset(iconPath, width: 24, height: 24),
-        ),
-        title: Text(capitalizeFirstLetter(
-          subjectName,),
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          ),
-        ),
-        subtitle: Text(capitalizeFirstLetter(
-          description),
-          style: TextStyle(fontSize: 14, color: Colors.grey[600]),
-        ),
-        trailing: TextButton(
+        child: Row(
+          children: [
+            Container(
+                height: 40,
+                width: 40,
+                padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                decoration: BoxDecoration(
+                    color: iconColor.withOpacity(0.2),
+                    // borderRadius: BorderRadius.circular(30),
+                    shape: BoxShape.circle),
+                child: Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: Image.asset(
+                    iconPath,
+                    height: 22,
+                    width: 20,
+                  ),
+                )),
+            SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                   capitalizeFirstLetter(subjectName),
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                
+                         Text(
+                          capitalizeFirstLetter(description),
+                          style: TextStyle(
+                            color: Colors.grey[600],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 10,
+                          ),
+                        ),
+                     
+                     
+                    ],
+                  ),),
+                   const Spacer(),
+          TextButton(
           onPressed: onEdit,
           child: Text(
             'Edit',
             style: TextStyle(color: Colors.black),
           ),
         ),
-      ),
-    );
-  }
-}
+                ],
+                 
+              ),
+            );
+          
+    
+      
+    
+  }}
+//   Widget build(BuildContext context) {
+//     return Card(
+//      padding: EdgeInsets.all(12),
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.only(
+//           topLeft: Radius.circular(topRadius ),
+//           topRight: Radius.circular(topRadius ),
+//           bottomLeft: Radius.circular(bottomRadius ),
+//           bottomRight: Radius.circular(bottomRadius ),
+//         ),
+//         side: BorderSide(
+//           color: Colors.grey.shade300,
+//           width: 1.0,
+//         ),
+//       ),
+//       child: ListTile(
+//         leading: CircleAvatar(
+//           backgroundColor: iconColor.withOpacity(0.2),
+//           child: Image.asset(iconPath, width: 24, height: 24),
+//         ),
+//         title: Text(capitalizeFirstLetter(
+//           subjectName,),
+//           style: TextStyle(
+//             fontWeight: FontWeight.bold,
+//             fontSize: 16,
+//           ),
+//         ),
+//         subtitle: Text(capitalizeFirstLetter(
+//           description),
+//           style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+//         ),
+//         trailing: TextButton(
+//           onPressed: onEdit,
+//           child: Text(
+//             'Edit',
+//             style: TextStyle(color: Colors.black),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
